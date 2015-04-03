4dbe1c13d0625f693cc74904885b3a077dc78825
Christina Holland <hsubox@gmail.com>
2015-04-02 20:31:42 -0700

    Mostly working, need to get variable numbers of procs working.

--- webworker/master.js
+++ webworker/master.js
@@ -12,6 +12,9 @@ var workers;
 var n;
 var total;
 var exitedWorkers;
+var startTime;
+var numWorkers;
+var nextColInQueue = 0;
 
 var processWorkerMessage = function (e) {
   retObj = e.data;
@@ -19,18 +22,24 @@ var processWorkerMessage = function (e) {
   var workerNumber = this._workerPlaceInArray;
   var workerPercent = retObj.percentage;
 
-  workers[this._workerPlaceInArray].$node.html('Worker ' + this._workerPlaceInArray + ' progress: ' + Math.round(workerPercent) + '%');
+  var percentString = Math.round(workerPercent) + "%";
+  workers[workerNumber].$bar_text.html(percentString);
+  var animationSpeed = (n-8)*20 + 20;
+  workers[workerNumber].$bar.animate({width: percentString}, animationSpeed);
 
   if ("total" in retObj) {
     total += retObj.total;
-    workers[this._workerPlaceInArray].$node.html('Worker ' + this._workerPlaceInArray + ' found: ' + retObj.total + ' solutions!');
-    workers[this._workerPlaceInArray].terminate();
+    workers[workerNumber].$text.html('Worker ' + workerNumber + ' found: ' + retObj.total + ' solutions!');
+    workers[workerNumber].terminate();
     exitedWorkers++;
   }
 
-  if(exitedWorkers === n) {
+  if(exitedWorkers === numWorkers) {
+    var endTime = performance.now();
+    var totalTime = (endTime - startTime);
+    $('h3').html('Results for n = ' + n + ':');
     var $result = $('<div class="result"></div>');
-    $result.html(total);
+    $result.html("Possible solutions: " + total + "<br>Elapsed time: " + totalTime.toFixed(1) + "ms.");
     $('h3').append($result);
   }
 };
@@ -38,19 +47,29 @@ var processWorkerMessage = function (e) {
 var main = function() {
   workers = [];
   n = +$('.n-field').val();
+  numWorkers = +$('.numWorkers-field').val();
   total = 0;
   exitedWorkers = 0;
   console.log('test');
   $('h3').html('Results:');
   $('.worker-container').html('');
-  for(var i = 0; i < n; i++) {
+  for(var i = 0; i < numWorkers; i++) {
+    startTime = performance.now();
     workers.push(new Worker('worker.js'));
     workers[i]._workerPlaceInArray = i;
     workers[i].postMessage({startingCol: i, n: n});
     workers[i].onmessage = processWorkerMessage;
     workers[i].$node = $('<div class="worker"></div>');
-    workers[i].$node.html('Worker ' + i + ' progress:');
+    workers[i].$text = $('<div class="worker-text">Worker ' + i + ' progress:</div>');
+    workers[i].$bar_container = $('<div class="worker-bar-container"></div>');
+    workers[i].$bar = $('<span class="worker-bar"></span>');
+    workers[i].$bar_text = $('<span class="worker-bar-text"></span>');
+    workers[i].$text.html('Worker ' + i + ' progress:');
     $('.worker-container').append(workers[i].$node);
+    workers[i].$node.append(workers[i].$text);
+    workers[i].$node.append(workers[i].$bar_container);
+    workers[i].$bar_container.append(workers[i].$bar);
+    workers[i].$bar_container.append(workers[i].$bar_text);
   }
 }
 
